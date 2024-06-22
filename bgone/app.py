6f561b10f2f8733c047a6ecb0a262def8987521c
Umeano Ibuchukwu <ibumeano@gmail.com>
2024-06-22 21:09:07 +0100

    added route handler

--- bgone/app.py
+++ bgone/app.py
@@ -7,6 +7,20 @@ app = Flask(__name__)
 
 @app.route('/', methods=['GET', 'POST'])
 def upload_image():
+    if request.method == 'POST':
+        if 'file' not in request.files:
+            return 'No image uploaded' , 401
+        file = request.files['file']
+        if file.filename == "":
+            return 'No file selected', 400
+        if file:
+            inputImage = Image.open(file.stream)
+            outputImage = remove(inputImage, post_process_mask=True)
+            imgIo = BytesIO()
+            outputImage.save(imgIo, 'PNG')
+            imgIo.seek(0)
+            return send_file(imgIo, mimetype="image/png", as_attachment=True, download_name=f"bgone_{file.filename}.png")
+        
     return render_template('index.html')
 
 if __name__ == '__main__':
